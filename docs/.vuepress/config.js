823e6ff4e0b228a7c24c2321f0e6c9e3177c4181
ysmc <494786209@qq.com>
2021-06-29 16:27:32 +0800

    更新时间

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,40 +3,13 @@ const headConf = require("./config/headConf")
 const pluginsConf = require('./config/pluginsConf')
 const sidebarConf = require('./config/sidebarConf')
 const navConf = require('./config/navConf')
-const moment = require('moment');
+
 
 module.exports = {
   title: "余生慢尝",
   description: '余生慢尝的笔记',
   head: headConf,
-  plugins: {
-    '@vuepress/last-updated': {
-      transformer: (timestamp) => {
-        moment.locale("zh-cn")
-        moment(timestamp).format('lll');
-      }
-    },
-    '@vuepress/pwa': {
-      serviceWorker: true,
-      updatePopup: {
-        message: "发现新内容可用",
-        buttonText: "刷新"
-      }
-    },
-    '@vssue/vuepress-plugin-vssue': {
-      // 设置 `platform` 而不是 `api`
-      platform: 'github-v4',
-
-      // 其他的 Vssue 配置
-      owner: 'ysmc-slg',
-      repo: 'docs',
-      clientId: 'dac3887952b01939c190',
-      clientSecret: '04045836314d98a2a91cf9aa5533c7339a2ae21a',
-      autoCreateIssue: true
-    },
-    '@vuepress/back-to-top': true,
-    "vuepress-plugin-auto-sidebar": {}
-  },
+  plugins: pluginsConf,
 
   themeConfig: {
     lastUpdated: '更新时间',
