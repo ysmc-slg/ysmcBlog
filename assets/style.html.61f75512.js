56cd09ba0ae1634747340e9b1dc4a4e4a9bedd61
ysmc-slg <ysmc-slg@users.noreply.github.com>
2022-06-22 09:23:13 +0000

    SpringSecurity认证 b7f7ed4af0f362fbe1ad9f6a325f3de9dfe390bb

---
+++ assets/style.html.61f75512.js
@@ -0,0 +1,97 @@
+import{e as n}from"./app.554c95d8.js";import{_ as s}from"./plugin-vue_export-helper.5a098b48.js";const a={},t=n(`<h1 id="\u7ED1\u5B9A\u6837\u5F0F" tabindex="-1"><a class="header-anchor" href="#\u7ED1\u5B9A\u6837\u5F0F" aria-hidden="true">#</a> \u7ED1\u5B9A\u6837\u5F0F</h1><div class="language-html ext-html"><pre class="language-html"><code><span class="token doctype"><span class="token punctuation">&lt;!</span><span class="token doctype-tag">DOCTYPE</span> <span class="token name">html</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>html</span><span class="token punctuation">&gt;</span></span>
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>head</span><span class="token punctuation">&gt;</span></span>
+		<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>meta</span> <span class="token attr-name">charset</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>UTF-8<span class="token punctuation">&quot;</span></span> <span class="token punctuation">/&gt;</span></span>
+		<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>title</span><span class="token punctuation">&gt;</span></span>\u7ED1\u5B9A\u6837\u5F0F<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>title</span><span class="token punctuation">&gt;</span></span>
+		<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>style</span><span class="token punctuation">&gt;</span></span><span class="token style"><span class="token language-css">
+			<span class="token selector">.basic</span><span class="token punctuation">{</span>
+				<span class="token property">width</span><span class="token punctuation">:</span> 400px<span class="token punctuation">;</span>
+				<span class="token property">height</span><span class="token punctuation">:</span> 100px<span class="token punctuation">;</span>
+				<span class="token property">border</span><span class="token punctuation">:</span> 1px solid black<span class="token punctuation">;</span>
+			<span class="token punctuation">}</span>
+			
+			<span class="token selector">.happy</span><span class="token punctuation">{</span>
+				<span class="token property">border</span><span class="token punctuation">:</span> 4px solid red<span class="token punctuation">;</span><span class="token punctuation">;</span>
+				<span class="token property">background-color</span><span class="token punctuation">:</span> <span class="token function">rgba</span><span class="token punctuation">(</span>255<span class="token punctuation">,</span> 255<span class="token punctuation">,</span> 0<span class="token punctuation">,</span> 0.644<span class="token punctuation">)</span><span class="token punctuation">;</span>
+				<span class="token property">background</span><span class="token punctuation">:</span> <span class="token function">linear-gradient</span><span class="token punctuation">(</span>30deg<span class="token punctuation">,</span>yellow<span class="token punctuation">,</span>pink<span class="token punctuation">,</span>orange<span class="token punctuation">,</span>yellow<span class="token punctuation">)</span><span class="token punctuation">;</span>
+			<span class="token punctuation">}</span>
+			<span class="token selector">.sad</span><span class="token punctuation">{</span>
+				<span class="token property">border</span><span class="token punctuation">:</span> 4px dashed <span class="token function">rgb</span><span class="token punctuation">(</span>2<span class="token punctuation">,</span> 197<span class="token punctuation">,</span> 2<span class="token punctuation">)</span><span class="token punctuation">;</span>
+				<span class="token property">background-color</span><span class="token punctuation">:</span> gray<span class="token punctuation">;</span>
+			<span class="token punctuation">}</span>
+			<span class="token selector">.normal</span><span class="token punctuation">{</span>
+				<span class="token property">background-color</span><span class="token punctuation">:</span> skyblue<span class="token punctuation">;</span>
+			<span class="token punctuation">}</span>
+
+			<span class="token selector">.atguigu1</span><span class="token punctuation">{</span>
+				<span class="token property">background-color</span><span class="token punctuation">:</span> yellowgreen<span class="token punctuation">;</span>
+			<span class="token punctuation">}</span>
+			<span class="token selector">.atguigu2</span><span class="token punctuation">{</span>
+				<span class="token property">font-size</span><span class="token punctuation">:</span> 30px<span class="token punctuation">;</span>
+				<span class="token property">text-shadow</span><span class="token punctuation">:</span>2px 2px 10px red<span class="token punctuation">;</span>
+			<span class="token punctuation">}</span>
+			<span class="token selector">.atguigu3</span><span class="token punctuation">{</span>
+				<span class="token property">border-radius</span><span class="token punctuation">:</span> 20px<span class="token punctuation">;</span>
+			<span class="token punctuation">}</span>
+		</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>style</span><span class="token punctuation">&gt;</span></span>
+		<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span> <span class="token attr-name">type</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>text/javascript<span class="token punctuation">&quot;</span></span> <span class="token attr-name">src</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>../js/vue.js<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span><span class="token script"></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">&gt;</span></span>
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>head</span><span class="token punctuation">&gt;</span></span>
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>body</span><span class="token punctuation">&gt;</span></span>
+		<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">id</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>root<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+			<span class="token comment">&lt;!-- \u7ED1\u5B9Aclass\u6837\u5F0F--\u5B57\u7B26\u4E32\u5199\u6CD5\uFF0C\u9002\u7528\u4E8E\uFF1A\u6837\u5F0F\u7684\u7C7B\u540D\u4E0D\u786E\u5B9A\uFF0C\u9700\u8981\u52A8\u6001\u6307\u5B9A --&gt;</span>
+			<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>basic<span class="token punctuation">&quot;</span></span> <span class="token attr-name">:class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>mood<span class="token punctuation">&quot;</span></span> <span class="token attr-name">@click</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>changeMood<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>{{name}}<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span> <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">/&gt;</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">/&gt;</span></span>
+
+			<span class="token comment">&lt;!-- \u7ED1\u5B9Aclass\u6837\u5F0F--\u6570\u7EC4\u5199\u6CD5\uFF0C\u9002\u7528\u4E8E\uFF1A\u8981\u7ED1\u5B9A\u7684\u6837\u5F0F\u4E2A\u6570\u4E0D\u786E\u5B9A\u3001\u540D\u5B57\u4E5F\u4E0D\u786E\u5B9A --&gt;</span>
+			<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>basic<span class="token punctuation">&quot;</span></span> <span class="token attr-name">:class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>classArr<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>{{name}}<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span> <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">/&gt;</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">/&gt;</span></span>
+
+			<span class="token comment">&lt;!-- \u7ED1\u5B9Aclass\u6837\u5F0F--\u5BF9\u8C61\u5199\u6CD5\uFF0C\u9002\u7528\u4E8E\uFF1A\u8981\u7ED1\u5B9A\u7684\u6837\u5F0F\u4E2A\u6570\u786E\u5B9A\u3001\u540D\u5B57\u4E5F\u786E\u5B9A\uFF0C\u4F46\u8981\u52A8\u6001\u51B3\u5B9A\u7528\u4E0D\u7528 --&gt;</span>
+			<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>basic<span class="token punctuation">&quot;</span></span> <span class="token attr-name">:class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>classObj<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>{{name}}<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span> <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">/&gt;</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">/&gt;</span></span>
+
+			<span class="token comment">&lt;!-- \u7ED1\u5B9Astyle\u6837\u5F0F--\u5BF9\u8C61\u5199\u6CD5 --&gt;</span>
+			<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>basic<span class="token punctuation">&quot;</span></span> <span class="token attr-name">:style</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>styleObj<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>{{name}}<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span> <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">/&gt;</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">/&gt;</span></span>
+			<span class="token comment">&lt;!-- \u7ED1\u5B9Astyle\u6837\u5F0F--\u6570\u7EC4\u5199\u6CD5 --&gt;</span>
+			<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>basic<span class="token punctuation">&quot;</span></span> <span class="token attr-name">:style</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>styleArr<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>{{name}}<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+		<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">&gt;</span></span>
+
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span> <span class="token attr-name">type</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>text/javascript<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span><span class="token script"><span class="token language-javascript">
+
+		<span class="token keyword">const</span> vm <span class="token operator">=</span> <span class="token keyword">new</span> <span class="token class-name">Vue</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+			el<span class="token operator">:</span><span class="token string">&#39;#root&#39;</span><span class="token punctuation">,</span>
+			data<span class="token operator">:</span><span class="token punctuation">{</span>
+				name<span class="token operator">:</span><span class="token string">&#39;\u5C1A\u7845\u8C37&#39;</span><span class="token punctuation">,</span>
+				mood<span class="token operator">:</span><span class="token string">&#39;normal&#39;</span><span class="token punctuation">,</span>
+				classArr<span class="token operator">:</span><span class="token punctuation">[</span><span class="token string">&#39;atguigu1&#39;</span><span class="token punctuation">,</span><span class="token string">&#39;atguigu2&#39;</span><span class="token punctuation">,</span><span class="token string">&#39;atguigu3&#39;</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
+				classObj<span class="token operator">:</span><span class="token punctuation">{</span>
+					atguigu1<span class="token operator">:</span><span class="token boolean">false</span><span class="token punctuation">,</span>
+					atguigu2<span class="token operator">:</span><span class="token boolean">false</span><span class="token punctuation">,</span>
+				<span class="token punctuation">}</span><span class="token punctuation">,</span>
+				styleObj<span class="token operator">:</span><span class="token punctuation">{</span>
+					fontSize<span class="token operator">:</span> <span class="token string">&#39;40px&#39;</span><span class="token punctuation">,</span>
+					color<span class="token operator">:</span><span class="token string">&#39;red&#39;</span><span class="token punctuation">,</span>
+				<span class="token punctuation">}</span><span class="token punctuation">,</span>
+				styleObj2<span class="token operator">:</span><span class="token punctuation">{</span>
+					backgroundColor<span class="token operator">:</span><span class="token string">&#39;orange&#39;</span>
+				<span class="token punctuation">}</span><span class="token punctuation">,</span>
+				styleArr<span class="token operator">:</span><span class="token punctuation">[</span>
+					<span class="token punctuation">{</span>
+						fontSize<span class="token operator">:</span> <span class="token string">&#39;40px&#39;</span><span class="token punctuation">,</span>
+						color<span class="token operator">:</span><span class="token string">&#39;blue&#39;</span><span class="token punctuation">,</span>
+					<span class="token punctuation">}</span><span class="token punctuation">,</span>
+					<span class="token punctuation">{</span>
+						backgroundColor<span class="token operator">:</span><span class="token string">&#39;gray&#39;</span>
+					<span class="token punctuation">}</span>
+				<span class="token punctuation">]</span>
+			<span class="token punctuation">}</span><span class="token punctuation">,</span>
+			methods<span class="token operator">:</span> <span class="token punctuation">{</span>
+				<span class="token function">changeMood</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">{</span>
+					<span class="token keyword">const</span> arr <span class="token operator">=</span> <span class="token punctuation">[</span><span class="token string">&#39;happy&#39;</span><span class="token punctuation">,</span><span class="token string">&#39;sad&#39;</span><span class="token punctuation">,</span><span class="token string">&#39;normal&#39;</span><span class="token punctuation">]</span>
+					<span class="token keyword">const</span> index <span class="token operator">=</span> Math<span class="token punctuation">.</span><span class="token function">floor</span><span class="token punctuation">(</span>Math<span class="token punctuation">.</span><span class="token function">random</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token operator">*</span><span class="token number">3</span><span class="token punctuation">)</span>
+					<span class="token keyword">this</span><span class="token punctuation">.</span>mood <span class="token operator">=</span> arr<span class="token punctuation">[</span>index<span class="token punctuation">]</span>
+				<span class="token punctuation">}</span>
+			<span class="token punctuation">}</span><span class="token punctuation">,</span>
+		<span class="token punctuation">}</span><span class="token punctuation">)</span>
+	</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">&gt;</span></span>
+	
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>html</span><span class="token punctuation">&gt;</span></span>
+</code></pre></div>`,2);function p(o,c){return t}var u=s(a,[["render",p]]);export{u as default};
